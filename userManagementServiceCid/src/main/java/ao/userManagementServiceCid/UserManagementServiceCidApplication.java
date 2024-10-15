42c74ffe658aea1982c2b6d6f1d017c4587bae22
JussyLeite <jussyleitefrancisco@gmail.com>
2024-10-15 09:59:11 +0100

    Adding usermanagement microservice

---
+++ userManagementServiceCid/src/main/java/ao/userManagementServiceCid/UserManagementServiceCidApplication.java
@@ -0,0 +1,13 @@
+package ao.userManagementServiceCid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementServiceCidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementServiceCidApplication.class, args);
+	}
+
+}
---
+++ userManagementServiceCid/src/test/java/ao/userManagementServiceCid/UserManagementServiceCidApplicationTests.java
@@ -0,0 +1,13 @@
+package ao.userManagementServiceCid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementServiceCidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
